132d7fa852b577c4552f00ad00e338262f6e3726
Lal <2101348@cognizant.com>
2024-01-12 11:20:18 +0530

    initial commit

---
+++ src/test/java/com/cts/springbootCIDemo/SpringbootCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.springbootCIDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
